75f9ef572e062b128ba8f37d5c19793f511fb2c1
Mark Winterbottom <mark@londonappdeveloper.com>
2017-11-19 16:57:57 +0000

    Added functions.

---
+++ functions.py
@@ -0,0 +1,9 @@
+
+def greet_customer(name, num_apples=6):
+    print('Hello, ' + name)
+    print('We have ' + str(num_apples) + ' apples in stock.')
+
+
+greet_customer('Mark')
+greet_customer('Brooke', 6)
+greet_customer('Greg', 2)
